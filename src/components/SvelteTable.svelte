bcace74d3e6ccb7865de4a18e37106e5702ba99e
Westborn <george@westborn.com.au>
2020-08-22 13:15:42 +1000

    feat: tried Svelte-Table app - issues with style

---
+++ src/components/SvelteTable.svelte
@@ -0,0 +1,167 @@
+<script>
+  console.log('here')
+  import { createEventDispatcher } from 'svelte'
+
+  const dispatch = createEventDispatcher()
+
+  export let columns
+  export let rows
+  export let sortBy = ''
+  export let sortOrder = 1
+  export let iconAsc = '▲'
+  export let iconDesc = '▼'
+  export let classNameTable = ''
+  export let classNameThead = ''
+  export let classNameTbody = ''
+  export let classNameSelect = ''
+  export let classNameRow = ''
+  export let classNameCell = ''
+
+  let sortFunction = () => ''
+  let showFilterHeader = columns.some(c => c.filterOptions !== undefined)
+  let filterValues = {}
+  let filterSettings = {}
+  let columnByKey = {}
+
+  columns.forEach(col => {
+    columnByKey[col.key] = col
+  })
+
+  $: c_rows = rows
+    .filter(r =>
+      Object.keys(filterSettings).every(f => {
+        let ret =
+          filterSettings[f] === undefined ||
+          // default to value() if filterValue() not provided in col
+          filterSettings[f] ===
+            (typeof columnByKey[f].filterValue === 'function'
+              ? columnByKey[f].filterValue(r)
+              : columnByKey[f].value(r))
+        return ret
+      })
+    )
+    .map(r => Object.assign({}, r, { $sortOn: sortFunction(r) }))
+    .sort((a, b) => {
+      if (a.$sortOn > b.$sortOn) return sortOrder
+      else if (a.$sortOn < b.$sortOn) return -sortOrder
+      return 0
+    })
+
+  const asStringArray = v =>
+    []
+      .concat(v)
+      .filter(v => typeof v === 'string' && v !== '')
+      .join(' ')
+
+  const calculateFilterValues = () => {
+    filterValues = {}
+    columns.forEach(c => {
+      if (typeof c.filterOptions === 'function') {
+        filterValues[c.key] = c.filterOptions(rows)
+      } else if (Array.isArray(c.filterOptions)) {
+        // if array of strings is provided, use it for name and value
+        filterValues[c.key] = c.filterOptions.map(val => ({ name: val, value: val }))
+      }
+    })
+  }
+
+  $: {
+    let col = columnByKey[sortBy]
+    if (col !== undefined && col.sortable === true && typeof col.value === 'function') {
+      sortFunction = r => col.value(r)
+    }
+  }
+
+  const updateSortOrder = colKey => {
+    if (colKey === sortBy) {
+      sortOrder = sortOrder === 1 ? -1 : 1
+    } else {
+      sortOrder = 1
+    }
+  }
+
+  const handleClickCol = (event, col) => {
+    updateSortOrder(col.key)
+    sortBy = col.key
+    dispatch('clickCol', { event, col, key: col.key })
+  }
+
+  const handleClickRow = (event, row) => {
+    dispatch('clickRow', { event, row })
+  }
+
+  const handleClickCell = (event, row, key) => {
+    dispatch('clickCell', { event, row, key })
+  }
+
+  if (showFilterHeader) {
+    calculateFilterValues()
+  }
+</script>
+
+<style>
+  table {
+    width: 100%;
+  }
+  .isSortable {
+    cursor: pointer;
+  }
+
+  tr th select {
+    width: 100%;
+  }
+</style>
+
+<table class={asStringArray(classNameTable)}>
+  <thead class={asStringArray(classNameThead)}>
+    {#if showFilterHeader}
+      <tr>
+        {#each columns as col}
+          <th>
+            {#if filterValues[col.key] !== undefined}
+              <select bind:value={filterSettings[col.key]} class={asStringArray(classNameSelect)}>
+                <option value={undefined} />
+                {#each filterValues[col.key] as option}
+                  <option value={option.value}>{option.name}</option>
+                {/each}
+              </select>
+            {/if}
+          </th>
+        {/each}
+      </tr>
+    {/if}
+    <slot name="header" {sortOrder} {sortBy}>
+      <tr>
+        {#each columns as col}
+          <th
+            on:click={e => handleClickCol(e, col)}
+            class={asStringArray([col.sortable ? 'isSortable' : null, col.headerClass])}>
+            {col.title}
+            {#if sortBy === col.key}{sortOrder === 1 ? iconAsc : iconDesc}{/if}
+          </th>
+        {/each}
+      </tr>
+    </slot>
+  </thead>
+  <tbody class={asStringArray(classNameTbody)}>
+    {#each c_rows as row, n}
+      <slot name="row" {row} {n}>
+        <tr
+          on:click={e => {
+            handleClickRow(e, row)
+          }}
+          class={asStringArray(classNameRow)}>
+          {#each columns as col}
+            <td
+              on:click={e => {
+                handleClickCell(e, row, col.key)
+              }}
+              class={asStringArray([col.class, classNameCell])}>
+              {@html col.renderValue ? col.renderValue(row) : col.value(row)}
+            </td>
+          {/each}
+        </tr>
+      </slot>
+    {/each}
+  </tbody>
+</table>
---
+++ src/components/volunteerTable.js
@@ -0,0 +1,47 @@
+export const columns = [
+  {
+    key: 'id',
+    title: 'ID',
+    value: (v) => v.id,
+  },
+  {
+    key: 'firstName',
+    title: 'First Name',
+    value: (v) => v.firstName,
+  },
+  {
+    key: 'lastName',
+    title: 'Surname',
+    value: (v) => v.lastName,
+  },
+  {
+    key: 'email',
+    title: 'eMail',
+    value: (v) => v.email,
+  },
+  {
+    key: 'phone',
+    title: 'Phone',
+    value: (v) => v.phone,
+  },
+  // {
+  //   key: 'notes2019',
+  //   title: 'notes2019',
+  //   value: (v) => v.notes2019,
+  // },
+  // {
+  //   key: 'notes2020',
+  //   title: 'notes2020',
+  //   value: (v) => v.notes2020,
+  // },
+  // {
+  //   key: 'activity2019',
+  //   title: 'activity2019',
+  //   value: (v) => v.activity2019,
+  // },
+  {
+    key: 'activity2020',
+    title: 'activity2020',
+    value: (v) => v.activity2020,
+  },
+]
